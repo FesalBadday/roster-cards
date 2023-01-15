94ac8d17c7733816ec57c3d0d974988fefa81b32
FesalBadday <fesal.n04@hotmail.com>
2023-01-14 17:32:47 -0700

    updated the loop with the new info

--- assets/js/client.mjs
+++ assets/js/client.mjs
@@ -1,38 +1,20 @@
 "use strict";
-// import students module
-import { students } from './students.mjs';
-
-// forEach loop to build social media and return them back the cards loop
-const getSocialMedia = (getSocialMedia) => {
-  let data = '';
-  getSocialMedia.forEach((socialMedia) => {
-    data += `<a href="${socialMedia.link}" target="_blank"><i class="bi bi-${socialMedia.name}"></i></a>`;
-  });
-  return data;
-};
-
-// forEach loop to build badges and return them back the cards loop
-const getBadges = (getBadges) => {
-  let data = '';
-  getBadges.forEach((badges) => {
-    data += `<i class="${badges.value}" title="${badges.name}"></i>`;
-  });
-  return data;
-};
+// import staff module
+import { staff } from './staff.mjs';
 
 // forEach loop to build the cards
-students.forEach(student => {
-  document.querySelector('section').innerHTML +=
-    `<article>
-      <div class="badges">
-        ${getBadges(student.badges)}
+staff.forEach(profile => {
+  document.querySelector('.container').innerHTML +=
+    `<div class="card">
+      <div class="lines"></div>
+      <div class="profile">
+        <img src="${profile.img}" alt="${profile.name}">
       </div>
-      <img src="assets/images/students/${student.img}" alt="${student.studentName}">
-      <div>
-        <h2>${student.studentName}</h2>
-        <p>${student.favLang}</p>
-        <p class="title">${student.bio}</p>
-        <p class="social">${getSocialMedia(student.socialMedia)}</p>
+      <div class="content">
+        <div class="info">
+          <h2>${profile.name}</h2>
+          <p>${profile.bio}</p>
+        </div>
       </div>
-    </article>`;
+  </div>`;
 });
